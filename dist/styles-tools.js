dcca62f2c02cdbe2086a4a74cb030f4e9937b1ed
potato <84955607+devkr-potato@users.noreply.github.com>
2022-02-06 20:39:57 +0900

    Create styles-tools.js

---
+++ dist/styles-tools.js
@@ -0,0 +1,11 @@
+const nitron_styleTools_badge = styles(`
+  font-size: 13px;
+  font-weight: bold;
+  line-height: 1;
+  display: inline-block;
+  padding: 3.6px 8px;
+  text-align: center;
+  white-space: nowrap;
+  border-radius: 10px;
+  vertical-align: baseline;
+`);
