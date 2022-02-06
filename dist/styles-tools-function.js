8f19904938959f6218834a0b27ec2626b010c2e6
potato <84955607+devkr-potato@users.noreply.github.com>
2022-02-06 20:40:37 +0900

    Create styles-tools-function.js

---
+++ dist/styles-tools-function.js
@@ -0,0 +1,18 @@
+function Color(color){
+  if(color == 'primary'){
+    color = styles(`background-color: ${nitron_styleTools_color.primary};`);
+  }else if(color == 'secondary'){
+    color = styles(`background-color: ${nitron_styleTools_color.secondary};`);
+  }else if(color == 'success'){
+    color = styles(`background-color: ${nitron_styleTools_color.success};`);
+  }else if(color == 'danger'){
+    color = styles(`background-color: ${nitron_styleTools_color.danger};`);
+  }else if(color == 'warning'){
+    color = styles(`background-color: ${nitron_styleTools_color.warning};`);
+  }else if(color == 'light'){
+    color = styles(`background-color: ${nitron_styleTools_color.light};`);
+  }else if(color == 'dark'){
+    color = styles(`background-color: ${nitron_styleTools_color.dark};`);
+  }
+  return color
+};
