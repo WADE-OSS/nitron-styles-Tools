57bdf4f95bb4b44275c858f7ccec7d935bdb14df
potato <84955607+devkr-potato@users.noreply.github.com>
2022-02-08 12:06:44 +0900

    Update styles-tools.js

--- dist/styles-tools.js
+++ dist/styles-tools.js
@@ -1,11 +1,123 @@
-const nitron_styleTools_badge = styles(`
-  font-size: 13px;
-  font-weight: bold;
-  line-height: 1;
-  display: inline-block;
-  padding: 3.6px 8px;
-  text-align: center;
-  white-space: nowrap;
-  border-radius: 10px;
-  vertical-align: baseline;
-`);
+customElements.define('dom-text', class extends HTMLElement {
+  connectedCallback() {
+    var TextClass = "margin: 0;";
+    if(this.getAttribute('size')){
+      TextClass += `font-size:${this.getAttribute('size')};`
+    }else{
+      TextClass += 'font-size: 18px;'
+    };
+    if(this.getAttribute('color')){
+      TextClass += this.getAttribute('color');
+    };
+    if(this.getAttribute('m')){
+      TextClass += `margin: ${this.getAttribute('m')};`
+    };
+    if(this.getAttribute('mt')){
+      TextClass += `margin-top: ${this.getAttribute('mt')};`
+    };
+    if(this.getAttribute('mb')){
+      TextClass += `margin-bottom: ${this.getAttribute('mb')};`
+    };
+    if(this.getAttribute('ml')){
+      TextClass += `margin-left: ${this.getAttribute('ml')};`
+    };
+    if(this.getAttribute('mr')){
+      TextClass += `margin-right: ${this.getAttribute('mr')};`
+    };
+    if(this.getAttribute('mx')){
+      TextClass += `margin-left: ${this.getAttribute('mx')}; margin-right: ${this.getAttribute('mx')};`
+    };
+    if(this.getAttribute('my')){
+      TextClass += `margin-top: ${this.getAttribute('my')}; margin-bottom: ${this.getAttribute('my')};`
+    };
+    if(this.getAttribute('p')){
+      TextClass += `padding: ${this.getAttribute('p')};`
+    };
+    if(this.getAttribute('pt')){
+      TextClass += `padding-top: ${this.getAttribute('pt')};`
+    };
+    if(this.getAttribute('pb')){
+      TextClass += `padding-bottop: ${this.getAttribute('pb')};`
+    };
+    if(this.getAttribute('pl')){
+      TextClass += `padding-left: ${this.getAttribute('pl')};`
+    };
+    if(this.getAttribute('pr')){
+      TextClass += `padding-right: ${this.getAttribute('pr')};`
+    };
+    if(this.getAttribute('px')){
+      TextClass += `padding-left: ${this.getAttribute('px')}; padding-right: ${this.getAttribute('px')};`
+    };
+    if(this.getAttribute('py')){
+      TextClass += `padding-top: ${this.getAttribute('py')}; padding-bottop: ${this.getAttribute('py')};`
+    };
+    if(this.getAttribute('style')){
+      TextClass += this.getAttribute('style')
+    };
+    this.outerHTML = `<p class="${styles(TextClass)}">${this.innerHTML}</p>`
+  };
+});
+
+customElements.define('dom-box', class extends HTMLElement {
+  connectedCallback() {
+    var TextClass = "margin: 0;";
+    if(this.getAttribute('color')){
+      TextClass += this.getAttribute('color');
+    };
+    if(this.getAttribute('m')){
+      TextClass += `margin: ${this.getAttribute('m')};`
+    };
+    if(this.getAttribute('mt')){
+      TextClass += `margin-top: ${this.getAttribute('mt')};`
+    };
+    if(this.getAttribute('mb')){
+      TextClass += `margin-bottom: ${this.getAttribute('mb')};`
+    };
+    if(this.getAttribute('ml')){
+      TextClass += `margin-left: ${this.getAttribute('ml')};`
+    };
+    if(this.getAttribute('mr')){
+      TextClass += `margin-right: ${this.getAttribute('mr')};`
+    };
+    if(this.getAttribute('mx')){
+      TextClass += `margin-left: ${this.getAttribute('mx')}; margin-right: ${this.getAttribute('mx')};`
+    };
+    if(this.getAttribute('my')){
+      TextClass += `margin-top: ${this.getAttribute('my')}; margin-bottom: ${this.getAttribute('my')};`
+    };
+    if(this.getAttribute('p')){
+      TextClass += `padding: ${this.getAttribute('p')};`
+    };
+    if(this.getAttribute('pt')){
+      TextClass += `padding-top: ${this.getAttribute('pt')};`
+    };
+    if(this.getAttribute('pb')){
+      TextClass += `padding-bottop: ${this.getAttribute('pb')};`
+    };
+    if(this.getAttribute('pl')){
+      TextClass += `padding-left: ${this.getAttribute('pl')};`
+    };
+    if(this.getAttribute('pr')){
+      TextClass += `padding-right: ${this.getAttribute('pr')};`
+    };
+    if(this.getAttribute('px')){
+      TextClass += `padding-left: ${this.getAttribute('px')}; padding-right: ${this.getAttribute('px')};`
+    };
+    if(this.getAttribute('py')){
+      TextClass += `padding-top: ${this.getAttribute('py')}; padding-bottop: ${this.getAttribute('py')};`
+    };
+    if(this.getAttribute('py')){
+      TextClass += `padding-top: ${this.getAttribute('py')}; padding-bottop: ${this.getAttribute('py')};`
+    };
+    if(this.getAttribute('width')){
+      TextClass += `width: ${this.getAttribute('width')};`
+    };
+    if(this.getAttribute('height')){
+      TextClass += `height: ${this.getAttribute('height')};`
+    };
+    if(this.getAttribute('style')){
+      TextClass += this.getAttribute('style')
+    };
+    this.outerHTML = `<div class="${styles(TextClass)}">${this.innerHTML}</div>`
+  };
+});
