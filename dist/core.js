0edfc289a405a64edba9f0da8765b9eca398ac21
potato <84955607+devkr-potato@users.noreply.github.com>
2022-02-18 21:19:08 +0900

    Update core.js

--- dist/core.js
+++ dist/core.js
@@ -12,16 +12,28 @@ class NitronStyles {
     customElements.define(componentName, class extends HTMLElement {
       connectedCallback() {   
         if(ComponentOptions.el){
-
           const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz'
           let className = `${elementName.replace('-',"_")}__`;
           for (let i = 0; i < 8; i++) {
             const rnum = Math.floor(Math.random() * chars.length)
             className += chars.substring(rnum, rnum + 1)
           };
+          if(this.getAttributeNames()) {
+            const AttrNames = this.getAttributeNames();
+            let optionsreturn = ComponentOptions.style;
+            AttrNames.forEach(attr => {
+              let val = this.getAttribute(attr);
+              optionsreturn = optionsreturn.replace(new RegExp(`\{\{ ?${attr} ?\}\}`,"g"), val);
+            });
+            ComponentOptions.style = optionsreturn;
+          }
+          if(ComponentOptions.props){
+            Object.keys(ComponentOptions.props).forEach(x => {
+              ComponentOptions.style = ComponentOptions.style.replace(new RegExp(`\{\{ ?${x} ?\}\}`,"g"), ComponentOptions.props[x]);
+            });
+          };
           nitron.styles(`.${className}`,ComponentOptions.style);
           this.outerHTML = nitron.createElement(ComponentOptions.el,{class:className},this.innerHTML);
-
         };
       };
     });
