31b41e9d3937e0893e0faddf97cc8094cb6c277e
potato <84955607+devkr-potato@users.noreply.github.com>
2022-02-19 16:52:42 +0900

    Update styles-tools.js

--- dist/styles-tools.js
+++ dist/styles-tools.js
@@ -1,61 +1,30 @@
-customElements.define('dom-text', class extends HTMLElement {
-  connectedCallback() {
-    var TextClass = "margin: 0;";
-    if(this.getAttribute('size')){
-      TextClass += `font-size:${this.getAttribute('size')};`
-    }else{
-      TextClass += 'font-size: 18px;'
-    };
-    if(this.getAttribute('color')){
-      TextClass += this.getAttribute('color');
-    };
-    if(this.getAttribute('m')){
-      TextClass += `margin: ${this.getAttribute('m')};`
-    };
-    if(this.getAttribute('mt')){
-      TextClass += `margin-top: ${this.getAttribute('mt')};`
-    };
-    if(this.getAttribute('mb')){
-      TextClass += `margin-bottom: ${this.getAttribute('mb')};`
-    };
-    if(this.getAttribute('ml')){
-      TextClass += `margin-left: ${this.getAttribute('ml')};`
-    };
-    if(this.getAttribute('mr')){
-      TextClass += `margin-right: ${this.getAttribute('mr')};`
-    };
-    if(this.getAttribute('mx')){
-      TextClass += `margin-left: ${this.getAttribute('mx')}; margin-right: ${this.getAttribute('mx')};`
-    };
-    if(this.getAttribute('my')){
-      TextClass += `margin-top: ${this.getAttribute('my')}; margin-bottom: ${this.getAttribute('my')};`
-    };
-    if(this.getAttribute('p')){
-      TextClass += `padding: ${this.getAttribute('p')};`
-    };
-    if(this.getAttribute('pt')){
-      TextClass += `padding-top: ${this.getAttribute('pt')};`
-    };
-    if(this.getAttribute('pb')){
-      TextClass += `padding-bottop: ${this.getAttribute('pb')};`
-    };
-    if(this.getAttribute('pl')){
-      TextClass += `padding-left: ${this.getAttribute('pl')};`
-    };
-    if(this.getAttribute('pr')){
-      TextClass += `padding-right: ${this.getAttribute('pr')};`
-    };
-    if(this.getAttribute('px')){
-      TextClass += `padding-left: ${this.getAttribute('px')}; padding-right: ${this.getAttribute('px')};`
-    };
-    if(this.getAttribute('py')){
-      TextClass += `padding-top: ${this.getAttribute('py')}; padding-bottop: ${this.getAttribute('py')};`
-    };
-    if(this.getAttribute('style')){
-      TextClass += this.getAttribute('style')
-    };
-    this.outerHTML = `<p class="${styles(TextClass)}">${this.innerHTML}</p>`
-  };
+styles.component('text',{
+  el:"p",
+  style:`
+    font-size: {{size}};
+    margin: {{m}};
+    margin-top: {{mt}};
+    margin-right: {{mr}};
+    margin-bottom: {{mb}};
+    margin-left: {{ml}};
+    margin-top: {{my}};
+    margin-bottom: {{my}};
+    margin-left: {{mx}};
+    margin-right: {{mx}};
+    padding: {{p}};
+    padding-top: {{pt}};
+    padding-right: {{pr}};
+    padding-bottom: {{pb}};
+    padding-left: {{pl}};
+    padding-top: {{py}};
+    padding-bottom: {{py}};
+    padding-left: {{px}};
+    padding-right: {{px}};
+  `,
+  props:{
+    m:"0",
+    size:"18px"
+  }
 });
 
 customElements.define('dom-box', class extends HTMLElement {
