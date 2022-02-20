5ff514dab544a4b86e40bf6c86bc9bc1ffab9bee
potato <84955607+devkr-potato@users.noreply.github.com>
2022-02-20 15:50:39 +0900

    Update core.js

--- dist/core.js
+++ dist/core.js
@@ -8,26 +8,27 @@ class NitronStyles {
       componentName = elementName
     }else{
       componentName = `dom-${elementName}`
-    };  
+    };
     customElements.define(componentName, class extends HTMLElement {
       connectedCallback() {
         if(ComponentOptions.el){
           const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz'
           let className = `${elementName.replace('-',"_")}__`;
+          let componentStyles = ComponentOptions.style;
           let props = {};
           for (let i = 0; i < 8; i++) {
             const rnum = Math.floor(Math.random() * chars.length)
             className += chars.substring(rnum, rnum + 1)
           };
-          if(this.getAttributeNames()) {
+          Object.assign(props,{class:className});
+          if(this.getAttributeNames()) {  
             const AttrNames = this.getAttributeNames();
-            let optionsreturn = ComponentOptions.style;
-            AttrNames.forEach(attr => {
-              if(optionsreturn.match(new RegExp(`\{\{ ?${attr} ?\}\}`,"g"))){
+            AttrNames.forEach(attr => { 
+
+              if(componentStyles.match(new RegExp(`\{\{ ?${attr} ?\}\}`,"g"))){
                 let val = this.getAttribute(attr);
-                optionsreturn = optionsreturn.replace(new RegExp(`\{\{ ?${attr} ?\}\}`,"g"), val);
+                componentStyles = componentStyles.replace(new RegExp(`\{\{ ?${attr} ?\}\}`,"g"),val);
               }else{
-                Object.assign(props,{class:className});
                 if(props[attr]){
                   props[attr] += ` ${this.getAttribute(attr)}`;
                 }else{
@@ -35,14 +36,13 @@ class NitronStyles {
                 };
               };
             });
-            ComponentOptions.style = optionsreturn;
-          }
+          };  
           if(ComponentOptions.props){
             Object.keys(ComponentOptions.props).forEach(x => {
-              ComponentOptions.style = ComponentOptions.style.replace(new RegExp(`\{\{ ?${x} ?\}\}`,"g"), ComponentOptions.props[x]);
+              componentStyles = componentStyles.replace(new RegExp(`\{\{ ?${x} ?\}\}`,"g"), ComponentOptions.props[x]);
             });
           };
-          nitron.styles(`.${className}`,ComponentOptions.style);
+          nitron.styles(`.${className}`,componentStyles);
           this.outerHTML = nitron.createElement(ComponentOptions.el,props,this.innerHTML);
         };
       };
