3fa9a30ae69cf6201b9c3eda443c6073bb3a4c2c
potato <84955607+devkr-potato@users.noreply.github.com>
2022-03-07 21:44:38 +0900

    Update core.js

--- dist/core.js
+++ dist/core.js
@@ -1,3 +1,12 @@
+/*
+ * Nitron.js v0.0.3 - core
+ *
+ * (c) 2022 WADE Open Source Software and Nitron Team. and its affiliates.
+ * Released under the MIT License.
+ * https://github.com/WADE-OSS/nitron-styles-Tools/blob/main/LICENSE
+ */
+
+
 class NitronStyles {
   constructor() {};
 
@@ -14,38 +23,54 @@ class NitronStyles {
     customElements.define(componentName, class extends HTMLElement {
       connectedCallback() {
         if(ComponentOptions.el){
-          const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz'
-          let className = `${elementName.replace('-',"_")}__`;
-          let componentStyles = ComponentOptions.style;
-          let props = {};
-          for (let i = 0; i < 8; i++) {
+        const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz'
+        let className = `${elementName.replace('-',"_")}__`;
+        let componentStyles = ComponentOptions.style;
+        let props = {};
+        for (let i = 0; i < 8; i++) {
             const rnum = Math.floor(Math.random() * chars.length)
             className += chars.substring(rnum, rnum + 1)
-          };
-          Object.assign(props,{class:className});
-          if(this.getAttributeNames()) {  
-            const AttrNames = this.getAttributeNames();
-            AttrNames.forEach(attr => { 
-
-              if(componentStyles.match(new RegExp(`\{\{ ?${attr} ?\}\}`,"g"))){
+        };
+        Object.assign(props,{class:className});
+        if(this.getAttributeNames()) {  
+          const AttrNames = this.getAttributeNames();
+          AttrNames.forEach(attr => {
+            if(componentStyles.match(new RegExp(`\{\{ ?${attr} ?\}\}`,"g"))){
                 let val = this.getAttribute(attr);
                 componentStyles = componentStyles.replace(new RegExp(`\{\{ ?${attr} ?\}\}`,"g"),val);
-              }else{
+            }else{
                 if(props[attr]){
-                  props[attr] += ` ${this.getAttribute(attr)}`;
+                props[attr] += ` ${this.getAttribute(attr)}`;
                 }else{
-                  props[attr] = `${this.getAttribute(attr)}`;
+                props[attr] = `${this.getAttribute(attr)}`;
                 };
+            };
+          });
+        };
+        if(ComponentOptions.event){
+          Object.keys(ComponentOptions.event).forEach(x => {
+            nitron.styles(`.${className}:${x}`,ComponentOptions.event[x]);
+          });
+        };
+        if(ComponentOptions.media){
+          Object.keys(ComponentOptions.media).forEach(x => {
+            let mediaquery = window.matchMedia(`screen and (${x})`);
+            mediaquery.addListener((a) => {
+              if(a.matches === true){
+                nitron.styles(`.${className}`,ComponentOptions.media[x]);
+              }else{
+                nitron.styles(`.${className}`,componentStyles);
               };
             });
-          };
-          if(ComponentOptions.props){
+          });
+        };
+        if(ComponentOptions.props){
             Object.keys(ComponentOptions.props).forEach(x => {
-              componentStyles = componentStyles.replace(new RegExp(`\{\{ ?${x} ?\}\}`,"g"), ComponentOptions.props[x]);
+            componentStyles = componentStyles.replace(new RegExp(`\{\{ ?${x} ?\}\}`,"g"), ComponentOptions.props[x]);
             });
-          };
-          nitron.styles(`.${className}`,componentStyles);
-          this.outerHTML = nitron.createElement(ComponentOptions.el,props,this.innerHTML);
+        };
+        nitron.styles(`.${className}`,componentStyles);
+        this.outerHTML = nitron.createElement(ComponentOptions.el,props,this.innerHTML);
         };
       };
     });
